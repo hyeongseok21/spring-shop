0718020257f49ad6c206c52be510e81a324b85ac
hyeongseok21 <wimpr0730@gmail.com>
2023-12-30 00:41:36 +0900

    implement product update

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,6 +20,20 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    // merge를 쓰면 안되고 아래와 같이 변경 감지를 해서, update친 필드만 set하도록 해야 한다.
+    // 그렇지 않으면 기존 데이터가 null로 바뀌는 경우가 생긴다. 방법이 있긴 하나 더 복잡해진다.
+    @Transactional
+    public Item updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // --- 아래와 같은 의미있는 method를 만들어 변경해서 추적할 수 있도록 해야 한다 --- //
+        // findItem.change(price, name, stockQuantity);
+        // findItem.addStock();
+        return findItem;
     }
 
     public List<Item> findItems() {
